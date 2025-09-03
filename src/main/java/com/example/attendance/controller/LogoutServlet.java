c77f86bfe867950952ae14ea6693203a570a9f53
oomorichie <oomorichie@10.95.3.209>
2025-09-03 15:33:06 +0900

    2025-09-03 7限 p71まで

--- src/main/java/com/example/attendance/controller/LogoutServlet.java
+++ src/main/java/com/example/attendance/controller/LogoutServlet.java
@@ -1,25 +1,24 @@
 package com.example.attendance.controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LogoutServlet
- */
+
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+									throws ServletException, IOException {
+		HttpSession session = req.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+		resp.sendRedirect("login.jsp");
 	}
-
 }
